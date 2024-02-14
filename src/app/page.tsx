1c3251178b52f211e89a9dfd8ee8aaae152d0764
zennon-sml <zennonsampaioml@gmail.com>
2024-02-14 09:25:00 -0300

    feat: chadcn ui carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Main from "./main";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 interface MoviesResponse {
   Search: Movie[];
@@ -19,8 +20,37 @@ interface Movie {
 export default async function Home() {
   const queryClient = new QueryClient()
   return (
+    <div>
+
     <QueryClientProvider client={queryClient}>
       <Main />
     </QueryClientProvider>
+    <ResizablePanelGroup
+    direction="horizontal"
+    className="max-w-md rounded-lg border"
+    >
+    <ResizablePanel defaultSize={50}>
+      <div className="flex h-[200px] items-center justify-center p-6">
+        <span className="font-semibold">One</span>
+      </div>
+    </ResizablePanel>
+    <ResizableHandle />
+    <ResizablePanel defaultSize={50}>
+      <ResizablePanelGroup direction="vertical">
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Two</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={75}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Three</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </ResizablePanel>
+  </ResizablePanelGroup>
+      </div>
   ) 
 }
