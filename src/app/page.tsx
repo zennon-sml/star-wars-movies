0b104792f5bf848c37ce088f424e7e84f5887117
zennon-sml <zennonsampaioml@gmail.com>
2024-02-20 16:56:54 -0300

    feat: Star Wars Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CarouselMovies from "./carousel";
+import { Star } from "lucide-react";
 import Image from "next/image";
 
 import {
@@ -34,18 +35,10 @@ export default async function Home() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="w-full h-screen bg-slate-900 flex flex-col justify-center items-center gap-4">
-      <div className="w-full bg-slate-950 h-24 flex flex-col items-center justify-center">
-      <NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>
-
+      <div className="w-full bg-slate-950 h-24 flex flex-row items-center justify-center text-slate-50 text-xl">
+      <Star className="text-yellow-300"/>
+      <h1>Star Wars </h1>
+      <Star className="text-yellow-300"/>
       </div>
       <div>
         <CarouselMovies />
